c11460076b9eb9eaeb22e4cc37522d175edb5c0c
Rory Tol <rorytol@gmail.com>
2022-12-08 00:01:56 +0100

    make the final counter file

--- Python_Code/word2gaus.py
+++ Python_Code/word2gaus.py
@@ -29,80 +29,95 @@ baroni = sum(results_neg_file, []) + sum(results_pos_file, [])
 baroni_set = set(baroni)
 
 # set wikidata to the subset of wikipedia dataset
-wikidata = datasets.load_dataset('wikipedia', '20200501.en')
-# make a subset
-wikidata = wikidata['train']
+# wikidata = datasets.load_dataset('wikipedia', '20200501.en')
+# # make a subset
+# wikidata = wikidata['train']
 
-print("start unpacking wiki data")
-# unpacking the wikidata
-texts = [x for x in wikidata['text']]
+# print("start unpacking wiki data")
+# # unpacking the wikidata
+# texts = [x for x in wikidata['text']]
 
-wiki_all_text = []
-chunk = int(len(texts)/10)
-import time
+# wiki_all_text = []
+# chunk = int(len(texts)/10)
+# import time
 
-for text in tqdm(texts[0:(1)*chunk]):
-    # Appending preprocessed text to the "all text" list
-    text = text_preprocessing(text)
-    wiki_all_text += text
+# for text in tqdm(texts[0:(1)*chunk]):
+#     # Appending preprocessed text to the "all text" list
+#     text = text_preprocessing(text)
+#     wiki_all_text += text
 
-print(f"chunk {1}")
-with open('wiki_preprocessed1.pickle', 'wb') as handle:
-    pickle.dump(wiki_all_text, handle, protocol=pickle.HIGHEST_PROTOCOL)
+# print(f"chunk {1}")
+# with open('wiki_preprocessed1.pickle', 'wb') as handle:
+#     pickle.dump(wiki_all_text, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-del(wiki_all_text)
-wiki_all_text = []
+# del(wiki_all_text)
+# wiki_all_text = []
 
-for text in tqdm(texts[chunk:(2)*chunk]):
-    # Appending preprocessed text to the "all text" list
-    text = text_preprocessing(text)
-    wiki_all_text += text
+# for text in tqdm(texts[chunk:(2)*chunk]):
+#     # Appending preprocessed text to the "all text" list
+#     text = text_preprocessing(text)
+#     wiki_all_text += text
 
-print(f"chunk {2}")
-with open('wiki_preprocessed2.pickle', 'wb') as handle:
-    pickle.dump(wiki_all_text, handle, protocol=pickle.HIGHEST_PROTOCOL)
+# print(f"chunk {2}")
+# with open('wiki_preprocessed2.pickle', 'wb') as handle:
+#     pickle.dump(wiki_all_text, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-del(wiki_all_text)
-wiki_all_text = []
+# del(wiki_all_text)
+# wiki_all_text = []
 
-for text in tqdm(texts[(2)*chunk:(3)*chunk]):
-    # Appending preprocessed text to the "all text" list
-    text = text_preprocessing(text)
-    wiki_all_text += text
+# for text in tqdm(texts[(2)*chunk:(3)*chunk]):
+#     # Appending preprocessed text to the "all text" list
+#     text = text_preprocessing(text)
+#     wiki_all_text += text
 
 
-print(f"chunk {3}")
-with open('wiki_preprocessed3.pickle', 'wb') as handle:
-    pickle.dump(wiki_all_text, handle, protocol=pickle.HIGHEST_PROTOCOL)
+# print(f"chunk {3}")
+# with open('wiki_preprocessed3.pickle', 'wb') as handle:
+#     pickle.dump(wiki_all_text, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-del(wiki_all_text)
-wiki_all_text = []
+# del(wiki_all_text)
+# wiki_all_text = []
 
-for text in tqdm(texts[(3)*chunk:(4)*chunk]):
-    # Appending preprocessed text to the "all text" list
-    text = text_preprocessing(text)
-    wiki_all_text += text
+# for text in tqdm(texts[(3)*chunk:(4)*chunk]):
+#     # Appending preprocessed text to the "all text" list
+#     text = text_preprocessing(text)
+#     wiki_all_text += text
 
-print(f"chunk {4}")
-with open('wiki_preprocessed4.pickle', 'wb') as handle:
-    pickle.dump(wiki_all_text, handle, protocol=pickle.HIGHEST_PROTOCOL)
+# print(f"chunk {4}")
+# with open('wiki_preprocessed4.pickle', 'wb') as handle:
+#     pickle.dump(wiki_all_text, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-del(wiki_all_text)
-wiki_all_text = []
+# del(wiki_all_text)
+# wiki_all_text = []
 
-for text in tqdm(texts[(4)*chunk:(5)*chunk]):
-    # Appending preprocessed text to the "all text" list
-    text = text_preprocessing(text)
-    wiki_all_text += text
+# for text in tqdm(texts[(4)*chunk:(5)*chunk]):
+#     # Appending preprocessed text to the "all text" list
+#     text = text_preprocessing(text)
+#     wiki_all_text += text
 
-print(f"chunk {5}")
+# print(f"chunk {5}")
 
-with open('wiki_preprocessed5.pickle', 'wb') as handle:
-    pickle.dump(wiki_all_text, handle, protocol=pickle.HIGHEST_PROTOCOL)
+# with open('wiki_preprocessed5.pickle', 'wb') as handle:
+#     pickle.dump(wiki_all_text, handle, protocol=pickle.HIGHEST_PROTOCOL)
+with open('wiki_preprocessed1.pickle', 'rb') as f:
+        wiki_preprocessed1 = pickle.load(f)
 
+with open('wiki_preprocessed2.pickle', 'rb') as f:
+        wiki_preprocessed2 = pickle.load(f)
+
+with open('wiki_preprocessed3.pickle', 'rb') as f:
+        wiki_preprocessed3 = pickle.load(f)
+
+with open('wiki_preprocessed4.pickle', 'rb') as f:
+        wiki_preprocessed4 = pickle.load(f)
+
+with open('wiki_preprocessed5.pickle', 'rb') as f:
+        wiki_preprocessed5 = pickle.load(f)
+
+wiki_preprocessed = wiki_preprocessed1 + wiki_preprocessed2 + wiki_preprocessed3 + wiki_preprocessed4 + wiki_preprocessed5
 
 print("create a wiki counter")
-wiki_count = Counter(tqdm(wiki_all_text))
+wiki_count = Counter(tqdm(wiki_preprocessed))
 
 baroniwiki_count = {k: wiki_count.get(k, None) for k in baroni_set}
 
