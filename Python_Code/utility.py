eacf6271cc39aff79271f19d920d0cc789e944fb
Rory Tol <rorytol@gmail.com>
2022-12-13 12:11:46 +0100

    update code

--- Python_Code/utility.py
+++ Python_Code/utility.py
@@ -49,15 +49,14 @@ def create_context_dict(all_text, window = 1):
     for i, word in tqdm(enumerate(all_text)):
         for w in range(window):
             # Getting the context that is ahead by *window* words
-            
             if i + 1 + w < len(all_text):
                 context_dict[word].append(all_text[(i + 1 + w)]) 
             # Getting the context that is behind by *window* words    
             if i - w - 1 >= 0:
                 context_dict[word].append(all_text[(i - w - 1)])
 
-
     return context_dict
+
 
 def cosine_similarity(a, b):
     nominator = np.dot(a, b)
@@ -70,3 +69,29 @@ def cosine_similarity(a, b):
     cosine_similarity = nominator / denominator
     
     return cosine_similarity
+
+#  In practice, it is also necessary to add a small ridge term 
+#  δ > 0 to the diagonal of the matrix to regularize and avoid 
+#  numerical problems when inverting - vilnis mccalumn
+
+def addDiagonal(matrix, x):
+    assert x < 1, f"x greater than 0 expected, got: {x}"
+    
+    for i in range(len(matrix)):
+        matrix[i][i] = matrix[i][i] + x
+    
+    return matrix
+
+
+def create_combined_subset(context_dict, results_neg_file, results_pos_file, combined_set):
+    combined_set_context_len = {}
+    
+    for i in combined_set:
+        combined_set_context_len[i] = len(context_dict[i])
+
+    combined_set_30plus = [x for x , key in combined_set_context_len.items() if key > 30]
+    
+    baroni_pos_subset = [x for x in results_pos_file if x[0] in combined_set_30plus and x[1] in combined_set_30plus]
+    baroni_neg_subset = [x for x in results_neg_file if x[0] in combined_set_30plus and x[1] in combined_set_30plus]
+
+    return baroni_pos_subset, baroni_neg_subset
