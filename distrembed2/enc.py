34ba6e24c2a0e9d4a0ca88c1fb5a653899809ff8
Rory Tol <rorytol@gmail.com>
2023-01-03 14:20:33 +0100

    pass param to enc

--- distrembed2/enc.py
+++ distrembed2/enc.py
@@ -103,7 +103,7 @@ def calculate_diag_kl(wordpair, embavg, vocab):
     # Get the mean vectors and covariance matrices for the two words in the word pair
     mean1, covariance_matrix1 = embavg.get_mean_covariance(vocab._tok_to_id.get(wordpair[0])) 
     mean2, covariance_matrix2 = embavg.get_mean_covariance(vocab._tok_to_id.get(wordpair[1])) 
-    n = len(covariance_matrix1[0])
+    n = int(100)
     # Create PyTorch multivariate normal distributions using the mean vectors and covariance matrices
     p = torch.distributions.multivariate_normal.MultivariateNormal(mean1, covariance_matrix=torch.eye(n, torch.diagonal(covariance_matrix1)))
     q = torch.distributions.multivariate_normal.MultivariateNormal(mean2, covariance_matrix=torch.eye(n, torch.diagonal(covariance_matrix2)))
