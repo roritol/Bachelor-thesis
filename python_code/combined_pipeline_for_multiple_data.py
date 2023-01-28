c307f580f37cd69a18baa781e42dce79d3dfff98
Rory Tol <rorytol@gmail.com>
2023-01-28 16:12:38 +0100

    import files

--- python_code/combined_pipeline_for_multiple_data.py
+++ python_code/combined_pipeline_for_multiple_data.py
@@ -125,7 +125,7 @@ def main():
     
    
     for j in tqdm(range(1,6)):
-        for i in tqdm([100]):
+        for i in tqdm(range(5, 26, 5)):
             df = pd.DataFrame(columns =['Max Context', 'KL Score AP', 'COS Score AP'])
             max_context = i 
 
