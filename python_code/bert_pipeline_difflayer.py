cb2181909e5a897c35b842c5ed6da51752064c6e
Rory Tol <rorytol@gmail.com>
2023-02-02 12:38:06 +0100

    ready for pull

--- python_code/bert_pipeline_difflayer.py
+++ python_code/bert_pipeline_difflayer.py
@@ -166,7 +166,7 @@ def main():
 
     n_batches = 1 + (len(wikidata[:]) // batch_size)
     # no_grad() turns off the requirement of gradients by the tensor output (reduce memory usage)
-    for layer_idx in [-5, -4, -3, -2, -1]:
+    for layer_idx in range(0, 12):
         with torch.no_grad():
             for k in trange(n_batches):
                 # grab a batch_size chunk from seqs (wiki data)
